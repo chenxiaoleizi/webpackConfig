9b5beb2632e26e3138982562a12b4523cb84d400
chenlei <791563973@qq.com>
2020-05-14 21:35:13 +0800

    test output.filename

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { consoleNum } from "./console"
+
+consoleNum()
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const path = require("path");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "bundle.js"
+        filename: "bundle.js",
+        // filename: "[name].bundle.js",
+        // filename: "[id].bundle.js",
+        // filename: "[hash].bundle.js"
+        path: path.resolve(__dirname, "dist/js")
     }
 }
