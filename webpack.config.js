7134c2d2a9282d5f342dab882faaff8d915f0524
chenlei <791563973@qq.com>
2020-05-15 19:58:42 +0800

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "bundle.js", 
+        filename: "bundle.[contenthash].js", 
         // filename: "[name].bundle.js",
         // filename: "[id].bundle.js",
         // filename: "[hash].bundle.js"
         path: path.resolve(__dirname, "dist/js")
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 }
